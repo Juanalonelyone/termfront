d2debf4e9ed306bd4b1e78074a5fb5b4eb3e62b0
徐锦华 <1019421426@qq.com>
2023-07-04 14:22:53 +0800

    完成摄像头在前端web的实时获取与展示视频流

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,54 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <img src="http://127.0.0.1:8000/video" className="video-js">
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  mounted() {
+    const videoElement = this.$refs.videoPlayer;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    // 创建 video.js 播放器实例
+    const player = videojs(videoElement);
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    // 发送请求获取视频流数据
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', videoUrl, true);
+    xhr.responseType = 'blob';
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    xhr.onload = () => {
+      // 获取服务器返回的视频流数据
+      const response = xhr.response;
+
+      // 创建一个 URL 对象，用于生成视频流的 URL
+      const videoUrl = URL.createObjectURL(response);
+
+      // 设置 video.js 播放器的 src 属性为视频流的 URL
+      player.src({
+        src: videoUrl,
+        type: 'video/mp4',
+      });
+
+      // 播放视频
+      player.play();
+    };
+
+    xhr.onerror = () => {
+      console.error('Error fetching the video stream.');
+    };
+
+    xhr.send();
+  },
+};
+</script>
+
+<style>
+.video-js {
+  width: 100%;
+  height: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import video from 'video.js'
+import 'video.js/dist/video-js.css'; // 导入video.js的CSS样式文件
 
 const app = createApp(App)
 
